b97e7b115e9a4186088cd562a01dd25c86e766a7
AlexMGlesser <alex.alexglesser@csu.fullerton.edu>
2026-02-18 18:11:53 -0800

    Created file and added a small structure

---
+++ lox.py
@@ -0,0 +1,18 @@
+import sys
+
+def runPrompt():
+    print("Usage: python3 lox.py [script]")
+
+def runFile(path):
+    with open(path, 'r') as file:
+        source = file.read()
+        
+
+def main():
+    if __name__ == "__main__":
+        if len(sys.argv) > 1:
+            runPrompt()
+        elif len(sys.argv) == 1:
+            runFile(sys.argv[0])
+        else:
+            runPrompt()
