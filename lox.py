063f776e6c6a115f0e4768e84eac409b48cfc8b0
AlexMGlesser <alex.alexglesser@csu.fullerton.edu>
2026-02-18 18:13:18 -0800

    Changes to main to be more efficient

--- lox.py
+++ lox.py
@@ -10,9 +10,8 @@ def runFile(path):
 
 def main():
     if __name__ == "__main__":
-        if len(sys.argv) > 1:
+        if len(sys.argv) > 1 or len(sys.argv) == 0:
             runPrompt()
-        elif len(sys.argv) == 1:
-            runFile(sys.argv[0])
         else:
-            runPrompt()
+            runFile(sys.argv[0])
+
