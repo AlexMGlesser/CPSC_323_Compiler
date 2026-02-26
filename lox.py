677299f9e309cd22bbbb88314a9d93cfe3e9b758
AlexMGlesser <alex.alexglesser@csu.fullerton.edu>
2026-02-25 17:44:18 -0800

    Added some functions and error handling

--- lox.py
+++ lox.py
@@ -1,17 +1,46 @@
+from pdb import run
 import sys
+from pathlib import Path
+
+had_error = False
+
+def usage():
+    print("Usage: lox [script]")
+    sys.exit(64)
 
 def runPrompt():
-    print("Usage: python3 lox.py [script]")
+    while True:
+        try:
+            line = input("> ")
+        except EOFError:
+            break
+        run(line)
 
 def runFile(path):
-    with open(path, 'r') as file:
-        source = file.read()
-        
+    text = Path(path).read_text(encoding=None)
+    run(text)
+
+def run(source):
+    scanner = Scanner(source)
+    tokens = scanner.scanTokens()
+
+    for token in tokens:
+        print(token)
+
+
+def error(line, message):
+    report(line, "", message)
+
+def report(line, where, message):
+    global had_error
+    print(f"[line {line}] Error{where}: {message}", file=sys.stderr)
+    had_error = True
 
 def main():
     if __name__ == "__main__":
-        if len(sys.argv) > 1 or len(sys.argv) == 0:
-            runPrompt()
-        else:
+        if len(sys.argv) > 1:
+            usage()
+        elif len(sys.argv) == 1:
             runFile(sys.argv[0])
-
+        else:
+            runPrompt()
